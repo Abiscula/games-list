6b99ebed21b927e8e6480284c99b90a527b3c043
Victor Abiscula <victorabiscula@live.com>
2024-01-15 20:29:54 -0300

    Criado setter para o atributo de usuário
    Se ao criar um novo usuário o gamer não possuir um userId será gerado internamente na classe Gamer.

--- src/main/kotlin/br/com/org/gameslist/model/Gamer.kt
+++ src/main/kotlin/br/com/org/gameslist/model/Gamer.kt
@@ -1,19 +1,35 @@
 package br.com.org.gameslist.model
 
+import kotlin.random.Random
+
 data class Gamer(var name: String, var email: String) {
     var birthDate: String? = null
     var user: String? = null
-    val userId: String? = null
+        set(value) {
+            field = value
+            if(userId.isNullOrBlank()) {
+                createUserId()
+            }
+        }
+    var userId: String? = null
+        private set
 
     constructor(name: String, email: String, birthDate: String, user: String): this(name, email) {
         this.birthDate = birthDate
         this.user = user
+        this.createUserId()
     }
 
     override fun toString(): String {
         return "Gamer(name='$name', email='$email', birthDate=$birthDate, user=$user, userId=$userId)"
     }
 
+    fun createUserId() {
+        val randomNumber = Random.nextInt(10000)
+        val tag = String.format("%04d", randomNumber)
+
+        this.userId = "$user#$tag"
+    }
 
 }
 
