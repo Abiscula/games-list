edf00e6b317aa26868292c19c67191368431a29d
Victor Abiscula <victorabiscula@live.com>
2024-01-14 23:14:11 -0300

    Criada classe Game

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,13 @@
+class Game {
+    var title = ""
+    var cover = ""
+    val description = ""
+
+    override fun toString(): String {
+        return "Meu jogo: \n" +
+                "Título: $title \n" +
+                "Capa: $cover \n" +
+                "Descrição: $description"
+    }
+
+}
