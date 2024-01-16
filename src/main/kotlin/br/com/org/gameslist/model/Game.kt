de240a7ab07802c159f553b6b228fc373843e68f
Victor Abiscula <victorabiscula@live.com>
2024-01-16 18:21:51 -0300

    Adicionado novo atributo a classe de Game
    Criado construtor para nova inicialização

--- src/main/kotlin/br/com/org/gameslist/model/Game.kt
+++ src/main/kotlin/br/com/org/gameslist/model/Game.kt
@@ -2,6 +2,12 @@ package br.com.org.gameslist.model
 
 data class Game(val title: String, val cover: String) {
     var description: String? = null
+    var price: Double = 0.0
+
+    constructor(title: String, cover: String, description: String, price: Double): this(title, cover) {
+        this.description = description
+        this.price = price
+    }
 
     override fun toString(): String {
         return "Meu jogo: \n" +
