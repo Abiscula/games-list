d24e2cf6c98ec1e6181d52f3f36c0a9a95565bc7
Victor Abiscula <victorabiscula@live.com>
2024-01-16 17:17:11 -0300

    Criado novo metodo no servico GameApi para retornar dados mockados dos gamers

--- src/main/kotlin/br/com/org/gameslist/services/GameApi.kt
+++ src/main/kotlin/br/com/org/gameslist/services/GameApi.kt
@@ -1,7 +1,11 @@
 package br.com.org.gameslist.services
 
+import br.com.org.gameslist.model.Gamer
 import br.com.org.gameslist.model.InfoGame
+import br.com.org.gameslist.model.InfoGamerJson
+import br.com.org.gameslist.utils.gamerCreate
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -26,6 +30,28 @@ class GameApi {
         val myInfoGame = gson.fromJson(json, InfoGame::class.java)
 
         return myInfoGame
+    }
+
+    fun gamersFind(): List<Gamer> {
+        val address = "https://raw.githubusercontent.com/jeniblodev/arquivosJson/main/gamers.json"
+
+        val client: HttpClient = HttpClient.newHttpClient();
+        val request: HttpRequest = HttpRequest.newBuilder()
+            .uri(URI.create(address))
+            .build()
+
+        val response = client
+            .send(request, HttpResponse.BodyHandlers.ofString())
+
+        val json = response.body()
+
+        val gson = Gson()
+        val gamerType = object : TypeToken<List<InfoGamerJson>>() {}.type
+        val gamerList: List<InfoGamerJson> = gson.fromJson(json, gamerType)
+
+        val convertGamerList = gamerList.map { infoGamerJson -> infoGamerJson.gamerCreate() }
+
+        return convertGamerList
     }
 
 }
