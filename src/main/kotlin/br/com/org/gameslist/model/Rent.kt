13225dfb43c45684fc8036d3139d7aa9df92dcb1
Victor Abiscula <victorabiscula@live.com>
2024-01-16 23:36:01 -0300

    Movido calculo de aluguel para class Plan

--- src/main/kotlin/br/com/org/gameslist/model/Rent.kt
+++ src/main/kotlin/br/com/org/gameslist/model/Rent.kt
@@ -2,9 +2,10 @@ package br.com.org.gameslist.model
 
 data class Rent(val player: Player,
                 val game: Game,
-                val period: RentPeriod,
-                val rentValue: Double = game.price * period.inDays
-) {
+                val period: RentPeriod) {
+
+    val rentValue: Double = player.plan.getRentValue(this)
+
     override fun toString(): String {
         return "Aluguel do ${game.title} por ${player.name} pelo valor de R$ $rentValue"
     }
