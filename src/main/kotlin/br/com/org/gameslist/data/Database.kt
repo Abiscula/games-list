96d475b1e37155015b44a6a400e2ba42f152b760
Victor Abiscula <victorabiscula@live.com>
2024-01-18 00:14:45 -0300

    Criado metodo para obter os jogos do db

--- src/main/kotlin/br/com/org/gameslist/data/Database.kt
+++ src/main/kotlin/br/com/org/gameslist/data/Database.kt
@@ -1,9 +1,16 @@
 package br.com.org.gameslist.data
 
+import br.com.org.gameslist.model.Game
+import java.math.BigDecimal
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 
+/*
+* O object é utilizado para criar objetos singleton.
+* Um objeto singleton é um instância única de uma classe, ou seja, apenas uma única instância dessa classe é
+* criada durante o ciclo de vida do programa.
+*/
 object Database {
     fun getConection(): Connection? {
         return try {
@@ -12,5 +19,33 @@ object Database {
             e.printStackTrace()
             null
         }
+    }
+
+    fun getGames(): List<Game> {
+        val connection = this.getConection()
+        val gamesList = mutableListOf<Game>()
+
+        if (connection != null) {
+            try {
+                val statement = connection.createStatement()
+                val result = statement.executeQuery("SELECT * FROM jogos")
+
+                while (result.next()) {
+                    val id = result.getInt("id")
+                    val title = result.getString("titulo")
+                    val cover = result.getString("capa")
+                    val description = result.getString("descricao")
+                    val price = result.getDouble("preco")
+
+                    val game = Game(title, cover, description, BigDecimal(price), id)
+                    gamesList.add(game)
+                }
+
+                statement.close()
+            } finally {
+                connection.close()
+            }
+        }
+        return gamesList
     }
 }
