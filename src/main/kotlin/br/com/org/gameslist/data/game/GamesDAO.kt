f15e7685e4fdccd98381df0269913ebde9f39ace
Victor Abiscula <victorabiscula@live.com>
2024-01-19 19:52:18 -0300

    Criada função utilitaria para instância de Game e GameEntity

--- src/main/kotlin/br/com/org/gameslist/data/game/GamesDAO.kt
+++ src/main/kotlin/br/com/org/gameslist/data/game/GamesDAO.kt
@@ -2,16 +2,18 @@ package br.com.org.gameslist.data.game
 
 import br.com.org.gameslist.data.DAO
 import br.com.org.gameslist.model.Game
+import br.com.org.gameslist.utils.toEntity
+import br.com.org.gameslist.utils.toModel
 import javax.persistence.EntityManager
 
 class GamesDAO(manager: EntityManager): DAO<Game, GameEntity>(manager, GameEntity::class.java) {
 
     override fun toEntity(obj: Game): GameEntity {
-        return GameEntity(obj.title, obj.cover, obj.price, obj.description)
+        return obj.toEntity()
     }
 
     override fun toModel(entity: GameEntity): Game {
-        return Game(entity.title, entity.cover, entity.price, entity.description, entity.id)
+        return entity.toModel()
     }
 
 }
--- src/main/kotlin/br/com/org/gameslist/utils/GameExtension.kt
+++ src/main/kotlin/br/com/org/gameslist/utils/GameExtension.kt
@@ -1,8 +1,17 @@
 package br.com.org.gameslist.utils
 
+import br.com.org.gameslist.data.game.GameEntity
 import br.com.org.gameslist.model.Game
 import br.com.org.gameslist.model.InfoGameJson
 
 fun InfoGameJson.gameCreate(): Game {
     return Game(this.titulo, this.capa, this.preco, this.descricao)
+}
+
+fun Game.toEntity(): GameEntity {
+    return GameEntity(this.title, this.cover, this.price, this.description, this.id)
+}
+
+fun GameEntity.toModel(): Game {
+    return Game(this.title, this.cover, this.price, this.description, this.id)
 }
