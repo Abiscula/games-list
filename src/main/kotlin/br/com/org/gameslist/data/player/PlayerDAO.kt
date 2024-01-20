47a55788392dcdd5b47797b8e3912c2840615810
Victor Abiscula <victorabiscula@live.com>
2024-01-19 21:25:42 -0300

    Criada função utilitaria para instância de Player e PlayerEntity

--- src/main/kotlin/br/com/org/gameslist/data/player/PlayerDAO.kt
+++ src/main/kotlin/br/com/org/gameslist/data/player/PlayerDAO.kt
@@ -8,13 +8,12 @@ import javax.persistence.EntityManager
 
 class PlayerDAO(manager: EntityManager): DAO<Player, PlayerEntity>(manager, PlayerEntity::class.java) {
     override fun toEntity(obj: Player): PlayerEntity {
-        return PlayerEntity(obj.name, obj.email, obj.birthDate, obj.user, obj.id, obj.plan.toEntity())
+        return obj.toEntity()
     }
 
     // apply: Adiciona um atributo que não está presente no constructor
     override fun toModel(entity: PlayerEntity): Player {
-        return Player(entity.name, entity.email, entity.birthDate, entity.user, entity.id)
-            .apply { plan = entity.plan.toModel() }
+        return entity.toModel().apply { plan = entity.plan.toModel() }
     }
 
 
--- src/main/kotlin/br/com/org/gameslist/utils/PlayerExtension.kt
+++ src/main/kotlin/br/com/org/gameslist/utils/PlayerExtension.kt
@@ -1,8 +1,17 @@
 package br.com.org.gameslist.utils
 
+import br.com.org.gameslist.data.player.PlayerEntity
 import br.com.org.gameslist.model.InfoPlayerJson
 import br.com.org.gameslist.model.Player
 
 fun InfoPlayerJson.playerCreate(): Player {
     return Player(this.nome, this.email, this.dataNascimento, this.usuario)
+}
+
+fun Player.toEntity(): PlayerEntity {
+    return PlayerEntity(this.name, this.email, this.birthDate, this.user, this.id, this.plan.toEntity())
+}
+
+fun PlayerEntity.toModel(): Player {
+    return Player(this.name, this.email, this.birthDate, this.user, this.id)
 }
